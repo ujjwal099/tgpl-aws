dab7797c2ac598af7c0935b6bacb064cce566731
Ujjwal Tiwari <ujjwaltiwari@administrators-MacBook-Pro.local>
2023-04-19 07:54:42 +0530

    fix:let-const-issue

--- index.mjs
+++ index.mjs
@@ -24,7 +24,7 @@ export const handler = async (event) => {
     ip = event.requestContext.identity.sourceIp;
   }
   console.log(ip);
-  const {
+  let {
     data,
     templateType,
     textSignature,

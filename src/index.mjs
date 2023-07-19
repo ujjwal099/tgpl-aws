c7aaacc5715ea2c63cd212c8c678c172bd3aba3a
Ujjwal Tiwari <ujjwal.tiwari@thriwe.com>
2023-07-19 12:29:23 +0530

    fix:template

--- src/index.mjs
+++ src/index.mjs
@@ -79,6 +79,7 @@ const createPdf = async (
     if (templateType == 4) {
       const tab = await browser.newPage();
       console.log("Template 4");
+      await page.setContent(htmlString, { waitUntil: "networkidle0" });
       await tab.addStyleTag({
         content: "@media print { section { page-break-after: always; } }",
       });

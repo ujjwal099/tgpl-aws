119c0124462296f77ed05116f1763aa644e4965b
Ujjwal Tiwari <ujjwal.tiwari@thriwe.com>
2023-07-25 11:21:22 +0530

    add:template

--- src/index.mjs
+++ src/index.mjs
@@ -6,7 +6,6 @@ import chromium from "@sparticuz/chromium";
 import path from "path";
 import { fileURLToPath } from "url";
 import puppeteer from "puppeteer-core";
-import "../NotoNaskhArabic-Regular.ttf";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
